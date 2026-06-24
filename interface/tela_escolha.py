818f6ad46001d69e3de340d4830628679343701c
devjuh98 <juliagalindo1212@gmail.com>
2026-06-24 14:18:24 -0300

    Adicionando scroll

--- interface/tela_escolha.py
+++ interface/tela_escolha.py
@@ -8,7 +8,7 @@ def tela_escolha(tela,jogador):
     fonte_titulo = pygame.font.SysFont(None, 60)
     fonte_texto = pygame.font.SysFont("Segoe UI Emoji", 40)
 
-    cabecalho = f" 👤 {jogador.nome}                                                                            🪙 {jogador.moedas} moedas"
+    cabecalho = f" 👤 {jogador.nome}                                                        🪙 {jogador.moedas} moedas"
 
     botoes = []
     largura_botao = 300
@@ -25,17 +25,23 @@ def tela_escolha(tela,jogador):
         y = inicio_y + linha * (altura_botao + espacamento_y)
         botoes.append((i+1, pygame.Rect(x, y, largura_botao, altura_botao)))
     
+    scroll_y = 0
     rodando = True
     while rodando:
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
                 rodando = False
             if evento.type == pygame.MOUSEBUTTONDOWN:
-                for i, rect in botoes:
-                    if rect.collidepoint(evento.pos):
-                        print(f"Período escolhido: {i}")
-                        return i, "tela_pergunta"
-        cabecalho = f" 👤 {jogador.nome}                   🪙 {jogador.moedas}"
+                if evento.button == 4:
+                    scroll_y += 40
+                elif evento.button == 5:
+                    scroll_y -= 40
+                else:
+                    for i, rect in botoes:
+                        rect_scroll = rect.move(0, scroll_y)
+                        if rect_scroll.collidepoint(evento.pos):
+                            print(f"Período escolhido: {i}º")
+                            return i, "tela_pergunta"
 
         tela.fill(constantes.BRANCO)
 
@@ -44,16 +50,17 @@ def tela_escolha(tela,jogador):
 
         # Cabeçalho
         texto_cabecalho = fonte_texto.render(cabecalho, True, constantes.BRANCO)
-        tela.blit(texto_cabecalho, (35, 100))
+        tela.blit(texto_cabecalho, (180, 100))
 
         # Título
         texto_titulo = fonte_titulo.render("ESCOLHA O PERÍODO", True, constantes.AMARELO)
-        tela.blit(texto_titulo, (450, 100))
+        tela.blit(texto_titulo, (430, 100))
 
         # Botões
         for i, rect in botoes:
-            pygame.draw.rect(tela, constantes.AZUL, rect, border_radius=10)
-            texto_botao = fonte_texto.render(str(i), True, constantes.BRANCO)
-            tela.blit(texto_botao, texto_botao.get_rect(center=rect.center))
+            rect_scroll = rect.move(0, scroll_y)
+            pygame.draw.rect(tela, constantes.AZUL, rect_scroll, border_radius=10)
+            texto_botao = fonte_texto.render(f"📚 {i}º período", True, constantes.BRANCO)
+            tela.blit(texto_botao, texto_botao.get_rect(center=rect_scroll.center))
         
         pygame.display.flip()

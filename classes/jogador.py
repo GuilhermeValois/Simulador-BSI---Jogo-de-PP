5869f2b5142337f6b3da9d4f03b6d52f13893785
Guilherme <guivas3902@gmail.com>
2026-06-25 11:40:04 -0300

    reconfigurando classe jogador

--- classes/jogador.py
+++ classes/jogador.py
@@ -6,12 +6,8 @@ class Jogador:
         self.nome = nome
         self.periodo = 1
         self.reprovacoes = 0
-        self.disciplinas_pendentes = [
-    Disciplina("Projeto Interdisciplinar para sistemas de informações 1", 1, "Matemática"),
-    Disciplina("Algoritmos e Programação", 1, "Programação"),
-    Disciplina("Fundamentos de Sistemas de Informação", 1, "BSI")
-]
-        self.moedas = 100
+        self.disciplinas_pendentes = []
+        self.moedas = 0
         self.energia = 100
         self.energia_max = 100
         self.reprovacoes = 0
